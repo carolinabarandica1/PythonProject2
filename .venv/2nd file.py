912e300961bd61caae76f71ada9061704f427f12
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-15 18:54:09 +0100

    new line

--- .venv/2nd file.py
+++ .venv/2nd file.py
@@ -1,2 +1,11 @@
 print('hello from another file')
 print('new message')
+
+
+print(not 7)
+print(True or False)
+print(2 or 3 or 4)
+print(False or 0 or 4 or True)
+print(1 and 2 and 3)
+print(1 and 0.0 and 4)
+print(False or 0 or None)
